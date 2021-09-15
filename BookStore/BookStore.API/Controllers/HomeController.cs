945aca08ec2359dd09b187a9659daecbdf241e23
Gilthayllor Sousa <gilthayllor@outlook.com>
2021-09-15 12:10:17 -0300

    BookStore.API

---
+++ BookStore/BookStore.API/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    /// <summary>
+    /// <see cref="HomeController"/>
+    /// </summary>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns><see cref="int"/></returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ BookStore/BookStore.API/Startup.cs
@@ -0,0 +1,85 @@
+using BookStore.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace BookStore.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(o =>
+            {
+                o.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Book API",
+                    Version = "v1"
+                });
+
+                var xFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xPath = Path.Combine(AppContext.BaseDirectory, xFile);
+
+                o.IncludeXmlComments(xPath);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Book API"));
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
