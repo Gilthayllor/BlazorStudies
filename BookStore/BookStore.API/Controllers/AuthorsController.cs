2dae6d9b3e4759afaf4d1a4a5662a08fd21e73de
Gilthayllor Brandão <gilthayllor@outlook.com>
2021-09-16 10:14:09 -0300

    Controllers

---
+++ BookStore/BookStore.API/Controllers/AuthorsController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using BookStore.API.Contracts.Repositories;
+using BookStore.API.Contracts.Services;
+using BookStore.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(IAuthorService authorService, IMapper mapper)
+        {
+            _authorService = authorService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get all authors.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<AuthorDTO>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<AuthorDTO>>> GetAll()
+        {
+            try
+            {
+                var authors = await _authorService.GetAll();
+                return Ok(authors);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get author by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(AuthorDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<AuthorDTO>> GetById([FromRoute] int id)
+        {
+            try
+            {
+                var author = await _authorService.Get(id);
+                if (author == null)
+                {
+                    return NotFound();
+                }
+
+                return author;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Create an author and save to database.
+        /// </summary>
+        /// <param name="author"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(AuthorDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<AuthorDTO>> CreateAuthor([FromBody] AuthorDTO author)
+        {
+            try
+            {
+                var newAuthor = await _authorService.Create(author);
+                return CreatedAtAction(nameof(GetById), new { id = newAuthor.Id }, newAuthor);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Update an author.
+        /// </summary>
+        /// <param name="author"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(typeof(AuthorDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<AuthorDTO>> UpdateAuthor([FromBody] AuthorDTO author)
+        {
+            try
+            {
+                await _authorService.Update(author);
+                return Ok(author);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> DeleteAuthor([FromRoute] int id)
+        {
+            try
+            {
+                await _authorService.Remove(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
