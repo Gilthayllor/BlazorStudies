1a387d6cf600d99dbc84b9d5f2ed7a928995bf83
Gilthayllor Brandão <gilthayllor@outlook.com>
2021-09-16 10:14:49 -0300

    Dependency Injection

---
+++ BookStore/BookStore.API/DependencyInjection/RepositoryDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.API.Contracts.Repositories;
+using BookStore.API.Implementations.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.DependencyInjection
+{
+    public static class RepositoryDependencyInjection
+    {
+        public static IServiceCollection ConfigureRepositoryDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ BookStore/BookStore.API/DependencyInjection/ServiceDependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.API.Contracts.Services;
+using BookStore.API.Implementations.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.DependencyInjection
+{
+    public static class ServiceDependencyInjection
+    {
+        public static IServiceCollection ConfigureServiceDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorService, AuthorService>();
+            //services.AddScoped<IAuthorRepository, AuthorRepository>();
+            //services.AddScoped<IBookRepository, BookRepository>();
+
+            return services;
+        }
+    }
+}
