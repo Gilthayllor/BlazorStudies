2540acd046e74605b30db8087cdb3d5dcf26ea1e
Gilthayllor Brandão <gilthayllor@outlook.com>
2021-09-16 10:13:56 -0300

    Services

---
+++ BookStore/BookStore.API/Contracts/Services/IAuthorService.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Contracts.Services
+{
+    public interface IAuthorService
+    {
+        Task<AuthorDTO> Create(AuthorDTO entity);
+
+        Task<AuthorDTO> Update(AuthorDTO entity);
+
+        Task<bool> Remove(int id);
+
+        Task<AuthorDTO> Get(int id);
+
+        Task<IEnumerable<AuthorDTO>> GetAll();
+    }
+}
